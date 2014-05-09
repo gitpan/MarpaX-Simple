f36839ecff0f857ef37621578b0d93b8849502d7
Steven Haryanto <stevenharyanto@gmail.com>
2014-05-09 17:10:06 +0000

    Import of SHARYANTO/MarpaX-Simple-0.01 from CPAN.
    gitpan-cpan-distribution: MarpaX-Simple
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/MarpaX-Simple-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/MarpaX/Simple.pm
@@ -0,0 +1,177 @@
+package MarpaX::Simple;
+
+use Marpa::R2;
+use UUID::Random;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(gen_parser);
+
+our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-05-10'; # DATE
+
+our %SPEC;
+
+$SPEC{gen_parser} = {
+    v => 1.1,
+    summary => 'Generate Marpa-based parser',
+    args => {
+        grammar => {
+            summary => '',
+            schema  => 'str*',
+            req => 1,
+            pos => 0,
+        },
+        actions => {
+            summary => '',
+            schema  => ['hash*', each_value => 'code*'],
+        },
+    },
+    result_naked => 1,
+    result => {
+        schema => 'code*',
+    },
+};
+sub gen_parser {
+    no strict 'refs';
+
+    my %args = @_;
+
+    my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar} });
+    my $pkg = __PACKAGE__ . '::gen' . substr(UUID::Random::generate(), 0, 8);
+    my $acts = $args{actions};
+    for (keys %$acts) {
+        *{"$pkg\::$_"} = $acts->{$_};
+    }
+
+    my $parser = sub {
+        my $input = shift;
+
+        my $recce = Marpa::R2::Scanless::R->new({
+            grammar => $grammar,
+            semantics_package => $pkg,
+        });
+        $recce->read(\$input);
+        my $valref = $recce->value;
+        if (!defined($valref)) {
+            die "No parse was found after reading the entire input\n";
+            # XXX show last expression
+        }
+        $$valref;
+    };
+
+    $parser;
+}
+
+1;
+# ABSTRACT: Generate Marpa-based parser
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+MarpaX::Simple - Generate Marpa-based parser
+
+=head1 VERSION
+
+This document describes version 0.01 of MarpaX::Simple (from Perl distribution MarpaX-Simple), released on 2014-05-10.
+
+=head1 SYNOPSIS
+
+ use MarpaX::Simple qw(gen_parser);
+
+ my $parser = gen_parser(
+     grammar => <<'EOG',
+ :start     ::= expr
+ expr       ::= num
+              | num '+' num    action => do_add
+ num        ~ [\d]+
+ :discard   ~ whitespace
+ whitespace ~ [\s]+
+ EOG
+     actions => {
+         do_add => sub { shift; $_[0] + $_[2] }
+     },
+ );
+
+ print $parser->('3 + 4'); # -> 7
+ print $parser->('3 + ');  # dies with parse error
+
+which is a shortcut for roughly this:
+
+ no strict 'refs';
+ use Marpa::R2;
+ my $grammar = Marpa::R2::Scanless::G->new({source => \$args{grammar}});
+ my $pkg = "MarpaX::Simple::gen" . some_random_value();
+ my $actions = $args{actions};
+ for (keys %$actions) {
+     ${"$pkg\::$_"} = $actions->{$_};
+ }
+ my $parser = sub {
+     my $input = shift;
+     my $recce = Marpa::R2::Scanless::R->new({
+         grammar => $grammar,
+         semantics_package => $pkg,
+     });
+ };
+
+=head1 DESCRIPTION
+
+This module tries to simplify the incantation of producing a parser using
+L<Marpa::R2> (the scanless interface) by reducing the process to a single
+function call: C<gen_parser>.
+
+=head1 FUNCTIONS
+
+
+=head2 gen_parser(%args) -> code
+
+Generate Marpa-based parser.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<actions> => I<hash>
+
+=item * B<grammar>* => I<str>
+
+=back
+
+Return value:
+
+=head1 SEE ALSO
+
+L<Marpa::R2>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/MarpaX-Simple>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-MarpaX-Simple>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=MarpaX-Simple>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
